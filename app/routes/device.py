02992a0ee77ba1a0e66d1260a25f1daa44c8b9f3
IbMoh <115637313+IbMoh@users.noreply.github.com>
2025-03-18 01:01:30 +0100

    finished device.py and fixed a bug in validatation

--- app/routes/device.py
+++ app/routes/device.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response, request, current_app, jsonify
+from flask import Blueprint, Response, request, current_app, jsonify, make_response
 from app.database.wrapper import Database
 from app.util import emit_event
 
@@ -10,21 +10,26 @@ def index(): return '👀 wat u lookin for m8'
 
 @deviceRouter.route('/device/create', methods=['POST'])
 def create_device():
-    # TODO: implement create device route
-    pass
+    body = request.json
+    Database.add_device(**body)
+    return jsonify(body), 201
 
 
-@deviceRouter.route('/device/<deviceId>/update', methods=['POST'])
+@deviceRouter.route('/device/<deviceId>', methods=['PATCH'])
 def update_device(deviceId):
     body = request.json
     updated_row = Database.update_device(deviceId, **body)
-    return jsonify({ "data": updated_row.toDict() })
+    return jsonify({ "data": updated_row.toDict() }), 201
 
 
-@deviceRouter.route('/device/<deviceId>/delete', methods=['POST'])
+@deviceRouter.route('/device/<deviceId>', methods=['DELETE'])
 def delete_device(deviceId):
-    # TODO: implement delete device route
-    pass
+    deleted_count = Database.remove_device(deviceId)
+    
+    if deleted_count > 0:
+        return jsonify({"message": "Device deleted successfully", "ID": deviceId}), 200
+    else:
+        return jsonify({"error": "Device not found"}), 404
 
 
 @deviceRouter.route('/logs', methods=['GET'])
