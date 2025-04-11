70597a70526441272807b074207280c82ed6f84e
idontreallywolf <nedim.kanat.lp@gmail.com>
2025-04-11 17:32:08 +0200

    write function to set JWT token variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 import os
+import secrets
 
 from flask_sse import sse
 from flask import Flask
 
 from flask_cors import CORS
+from dotenv import set_key
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +14,8 @@ def create_app():
         raise Exception("SECRET_APP_KEY not found in environment variables.")
 
     app.secret_key = os.environ.get("SECRET_APP_KEY")
+
+    checkJWTtokens()
 
     if (("SQLALCHEMY_DATABASE_URI" not in os.environ) or ("SQLALCHEMY_DATABASE_URI" == "")):
         print("SQLALCHEMY_DATABASE_URI not found. Using default --> sqlite:///project.db")
@@ -30,3 +34,21 @@ def create_app():
     )
 
     return app
+
+
+def checkJWTtokens():
+    TOKENS = ["JWT_ACCESS_TOKEN", "JWT_REFRESH_TOKEN"]
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) + "/.."
+
+    for token in TOKENS:
+        variableExists = token in os.environ
+
+        if variableExists and len(os.environ.get(token)) > 0:
+            print(f" 🟢 — {token} found.")
+            continue
+
+        print(f" 🔴 — {token} not found. Generating new ...")
+        newToken = secrets.token_hex(32)
+        os.environ[token] = newToken
+        set_key(dotenv_path=ROOT_DIR + "/.env", key_to_set=token, value_to_set=newToken)
+        print(f" 🟢 — Generated new {token}")
