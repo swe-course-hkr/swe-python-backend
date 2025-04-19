d94645505f4410308f11be3edd11d73a211ddab0
IbMoh <115637313+IbMoh@users.noreply.github.com>
2025-04-19 23:52:35 +0200

    finished docs for my part

--- app/routes/device.py
+++ app/routes/device.py
@@ -1,3 +1,31 @@
+'''
+Device related routes
+
+Routes:
+
+index -> main index route should not be accessed by user
+
+get_all_devices -> route to fetch all devices in the database
+
+create_device -> route to create a new device, user must be logged in to do so
+
+update_device -> route to update an existing device in the database, based on a device ID
+
+delete_device -> route to delete a device using a device ID
+
+get_logs -> route to get server logs (unit team wanted this route)
+
+SocketIO & Serial Communication Functions:
+
+command() -> Emits a message received from the serial device to clients via SocketIO.
+
+serial_ports() -> Detects and filters available COM ports. On Windows, filters for "CP210x" devices.
+
+ser() -> Opens a serial connection using a detected COM port and reads data continuously from the hardware.
+
+received() -> SocketIO event handler that receives data from clients and writes it to the serial device.
+'''
+
 from flask import Blueprint, request
 from app.database.wrapper import Database
 from app.socket import socketio
@@ -6,12 +34,13 @@ import serial.tools.list_ports
 import sys
 import glob
 
+
+''' Define a Blueprint for device-related routes '''
 deviceRouter = Blueprint('device', __name__)
 
 @deviceRouter.route('/')
 def index():
     return "what are you looking at"
-
 
 @deviceRouter.route('/device/all', methods=['GET'])
 def get_all_devices():
@@ -73,7 +102,10 @@ def get_logs():
         'logs': [log.toDict() for log in Database.fetch_logs()]
     })
 
-
+'''
+Create a Serial instance.
+Essentially turning the server into a serial monitor
+'''
 serialInst = serial.Serial()
 
 def command(command):
--- app/socket.py
+++ app/socket.py
@@ -10,7 +10,7 @@ cors_allowed_origins is set to allow connections from specific clients.
 socketio = SocketIO(cors_allowed_origins=[
     "http://192.168.87.116:3000", 
     "http://172.27.176.1:3000", 
-    "http://192.168.50.234:3000", 
-    "*"
+    "http://192.168.50.234:3000",
+    "http://localhost:3000",
     ]
 )
