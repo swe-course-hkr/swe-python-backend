b8a1d981fe117c980ae585ab3fb2cbdee66482b2
idontreallywolf <nedim.kanat.lp@gmail.com>
2025-05-05 07:20:32 +0200

    remove unit test for create-log

--- tests/test_log.py
+++ tests/test_log.py
@@ -7,21 +7,7 @@ from . import client, app
 
 #add log
 def test_addLog(client):
-    jsonPost = {"log_level":"info",
-            "action":"unittest",
-            "user_id":1,
-            "device_id":1}
-    response = client.post('/logs/create', json=jsonPost)
-
-    strJson = response.data.decode('utf-8')
-    jsonResponse = json.loads(strJson)
-
-    #check data
-    for v in jsonPost:
-       if v == "log_level":  var = "level"
-       else: var = v
-       assert jsonResponse["data"][var] == jsonPost[v]
-    
-    #check statuscode
-    assert response.status_code == 201
-
+    # we don't need to have an endpoint for creating logs,
+    # we should have tests for internal functions (not exposed to clients),
+    # which are related to managing logs
+    assert True
