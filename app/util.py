df61d030047f0cb35b49d9e445c7d533cb6f8599
Nedkan <nedim.kanat.lp@gmail.com>
2025-05-08 14:16:10 +0200

    check pw length (#44)

--- app/util.py
+++ app/util.py
@@ -300,6 +300,9 @@ class Middleware:
 
             if not password:
                 return errorResponse("Password cannot be empty", 400)
+            
+            if len(password) < 8:
+                return errorResponse("Password needs to be at least 8 characters long")
 
             if not re.match(pattern, password):
                 return errorResponse(
